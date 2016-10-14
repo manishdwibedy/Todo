f7ac90dc0d7fd7298328376719dbc8bc1b63fc53
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-10-14 10:28:02 -0700

    Creating a model class for todo.

---
+++ ToDo/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  ToDo
+//
+//  Created by Manish Dwibedy on 10/14/16.
+//  Copyright © 2016 Manish Dwibedy. All rights reserved.
+//
+
+import UIKit
+
+class Todo{
+    dynamic var text = ""
+}
